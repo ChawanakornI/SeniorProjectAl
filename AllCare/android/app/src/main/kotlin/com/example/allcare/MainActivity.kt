7efe865ec41b42494ae1ca8f4040505a45350d76
Tonnam K. <107524401+iamtonnam@users.noreply.github.com>
2026-02-11 16:13:56 +0700

    ฯOld ver

--- AllCare/android/app/src/main/kotlin/com/example/allcare/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.allcare
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- AllCare/backserver/tests/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Test package for AllCare backserver."""
